5ef273675bbd590f8867fcaa01b9a50b07157435
RobiLono <robilono@hawaii.edu>
2020-03-01 22:19:58 -0800

    Ex4.js

--- Lab7/Ex4.js
+++ Lab7/Ex4.js
@@ -4,6 +4,23 @@ likesBeer = true;
 
 console.log(name);
 
-if ( !likesBeer ){console.log('beer is something I dislike');}
-if (likesBeer){console.log('I like beer!');}
+if (!likesBeer) { console.log('beer is something I dislike'); }
+if (likesBeer) { console.log('I like beer!'); }
+
+
+if (quantity > 1) {
+    price = 100;
+    if (quantity >= 10) {
+        price = 50;
+    }
+
+    if (quantity >= 25) {
+        price = 35;
+    }
+}
+else
+    price = "no purchase";
+
+console.log(quantity + ' products will cost ' + price + ' each.');
+
 
